31677539adc3ec3a1dd62d407a78d4bdcc25738e
Apurva Panse <apoorvapanse@ucla.edu>
2017-06-07 10:50:18 -0700

    Added unreferenced blocks

--- lab3b.py
+++ lab3b.py
@@ -111,25 +111,30 @@ def helper_offset(inx):
   elif inx == 14:
   	return 65804
   else:
-  	return 0
+  	return inx
 
 def helper_indirect(inx):
   if (inx == 12):
   	return "INDIRECT "
   elif (inx == 13):
-  	return "DOUBLY INDIRECT "
+  	return "DOUBLE INDIRECT "
   elif (inx == 14):
-  	return "TRIPPLY INDIRECT "
+  	return "TRIPPLE INDIRECT "
   else:
   	return ""
 
 def invalid_blocks(superblock):
   for inode in inode_list:
 	for inx,bp in enumerate(inode.block_pointers):
-	  if int(bp) < 0 or int(bp) > superblock.tot_blocks:
+	  if int(bp) < 0 or int(bp) >= superblock.tot_blocks:
 	  	indirect = helper_indirect(int(inx))
 	  	offset = helper_offset(int(inx))
 	  	print "INVALID " + indirect + "BLOCK " + str(int(bp)) + " IN INODE " + str(inode.inode_num) + " AT OFFSET " + str(offset)
+
+def reserved_blocks(superblock, group):
+  upper_range = group.first_inode + (superblock.inode_size * superblock.tot_inodes) / superblock.block_size
+  for block in range(1, upper_range):
+  	print block
 
 def allocated_blocks():
   # check all of the inodes in inode_list[i].block_pointers and see if they are
@@ -184,15 +189,31 @@ def duplicate_blocks():
 		unique_blocks.append(Dup_block(inode.block_pointers[inx], inode.inode_num, offset, indirect))
   for dups in dup_blocks:
   	print "DUPLICATE " + dups.indirect + "BLOCK " + str(dups.block_num) + " IN INODE " + str(dups.inode_num) + " AT OFFSET " + str(dups.offset)
-  	
-	  
 
+def unreferenced_blocks(superblock, group):
+  lower_range = group.first_inode + (superblock.inode_size * superblock.tot_inodes) / superblock.block_size
+  for block in range(lower_range, superblock.tot_blocks):
+  	referenced = False
+	for bp in free_blocks:
+	  if bp == block:
+	  	referenced = True
+	for inode in inode_list:
+	  for bp in inode.block_pointers:
+		if int(bp) == block:
+		  referenced = True
+	for indirect in indirect_list:
+	  if indirect.block_num_ref == block:
+	  	referenced = True
+	  if indirect.block_num_indirect == block:
+	  	referenced = True
+	if referenced == False:
+	  print "UNREFERENCED BLOCK " + str(int(block))
 
 def inode_audit(superblock):
   #if there's only 1 group, tot_blocks could be < blocks per group 
   if(superblock.tot_blocks < superblock.blocks_per_group):
     total_inodes = superblock.inodes_per_group
-  else:#this would need to be fixed for multiple groups. 
+  else:#TODO: this would need to be fixed for multiple groups. 
     total_inodes = (superblock.tot_blocks / superblock.blocks_per_group) * (superblock.inodes_per_group)
   #check allocated inodes
   used_inodes = []
@@ -211,36 +232,12 @@ def inode_audit(superblock):
           print "UNALLOCATED INODE",inode,"NOT ON FREELIST"
       
 def check_directory_two(superblock):
-  parent = {}
-  if( superblock.tot_blocks < superblock.blocks_per_group):
-    total_inodes = superblock.inodes_per_group
-  else:#this would need to be fixed for multiple groups.                                                                                             
-    total_inodes = (superblock.tot_blocks / superblock.blocks_per_group) * (superblock.inodes_per_group)
-  for dir in dirent_list:
-    if dir.inode_num < total_inodes: #Make sure its not invalid
-      if dir.inode_num in inode_number_list: #NOT unallocated inode                                                                                  
-        if dir.name[1:-1] is not '.' or dir.name[1:-1]is not '..':
-          parent[dir.inode_num] = dir.parent_inode #map key = inode number. #map value = its parent
-
-  for dir in dirent_list:
-    if dir.name[1:-1] == '..' and dir.parent_inode == 2 and dir.inode_num != 2: #special case for root 2. it's parent needs to be itself
-      print "DIRECTORY INODE 2 NAME '..' LINK TO INODE",dir.inode_num,"SHOULD BE 2"
-    if dir.name[1:-1] == '.':
-      if dir.inode_num is not dir.parent_inode:
-        print "DIRECTORY INODE",dir.inode_num,"NAME '.' LINK TO INODE",dir.parent_inode,"SHOULD BE",dir.inode_num
-    elif  dir.name[1:-1] == '..' and dir.inode_num != 2:
-      if dir.parent_inode is not parent[dir.inode_num] and dir.inode_num is not dir.parent_inode:
-        print "DIRECTORY INODE",dir.inode_num,"NAME '..' LINK TO INODE",dir.parent_inode,"SHOULD BE",parent[dir.inode_num]
-      
-
-"""  one_dot = []
+  one_dot = []
   two_dot = []
   for dir in dirent_list:
     if dir.name[1:-1] is '.':
-      print dir.parent_inode,"has ."
-      one_dot.append(dir.parent_inode)
+      one_dot.append(dir.parent_inode) #TODO: should this be inode num of referenced file??
     elif dir.name[1:-1] == '..':
-      print dir.parent_inode,"has.."
       two_dot.append(dir.parent_inode)
   
   for dir in dirent_list: #make sure each entry is in one dot and two dot
@@ -251,36 +248,34 @@ def check_directory_two(superblock):
       print "DIRECTORY INODE",dir.parent_inode,"NAME '.' LINK TO INODE",dir.inode_num,"SHOULD BE",dir.parent_inode
     elif dir.parent_inode not in two_dot:
       print "DIRECTORY INODE",dir.parent_inode,"NAME '..' LINK TO INODE",dir.inode_num,"SHOULD BE",dir.parent_inode
-"""
 
 def check_inode_linkcount(superblock):
   #total number of inodes
   #if there's only 1 group, tot_blocks could be < blocks per group                                                                               
   if( superblock.tot_blocks < superblock.blocks_per_group):
     total_inodes = superblock.inodes_per_group
-  else:#this would need to be fixed for multiple groups.
+  else:#TODO: this would need to be fixed for multiple groups.
     total_inodes = (superblock.tot_blocks / superblock.blocks_per_group) * (superblock.inodes_per_group)
   inode_to_link = {}
   for dir in dirent_list:
     inode = dir.inode_num
     if inode > total_inodes:
       #invalid inode
-      print "DIRECTORY INODE",dir.parent_inode,"NAME",dir.name,"INVALID INODE",dir.inode_num 
+      print "DIRECTORY INODE",inode,"NAME",dir.name,"INVALID INODE",dir.inode_num 
+    if inode in inode_to_link:
+      inode_to_link[inode] += 1#already in dictory so increment link counter
     else:
-      if inode in inode_to_link:
-        inode_to_link[inode] += 1#already in dictory so increment link counter
-      else:
-        inode_to_link[inode] = 1#init link counter dictionary entry
-        if inode not in inode_number_list: #unallocated inode
-          print "DIRECTORY INODE",dir.parent_inode,"NAME",dir.name,"UNALLOCATED INODE",dir.inode_num
+      inode_to_link[inode] = 1#init link counter dictionary entry
+    if inode not in inode_number_list: #unallocated inode
+      print "DIRECTORY INODE",inode,"NAME",dir.name,"UNALLOCATED INODE",dir.inode_num
   #check dictionary with inode link count values
   for inode in inode_list:
     if inode.inode_num in inode_to_link:
       if inode.link_count != inode_to_link[inode.inode_num]:
-        print "INODE",inode.inode_num,"HAS",inode_to_link[inode.inode_num],"LINKS BUT LINKCOUNT IS",inode.link_count
+        print "INODE",inode.inode_num,"HAS",inode_to_link[inode.inode_num],"BUT LINKCOUNT IS",inode.link_count
     else: 
       if inode.link_count != 0:
-        print "INODE",inode.inode_num,"HAS 0 LINKS BUT LINKCOUNT IS",inode.link_count
+        print "INODE",inode.inode_num,"HAS 0 BUT LINKCOUNT IS",inode.link_count
     
 def main():
   f = open(sys.argv[1], 'r')
@@ -315,8 +310,9 @@ def main():
   #Block consistency audits
   #block_audit()
   #Inode audits
+  reserved_blocks(superblock, group)
   inode_audit(superblock)
-#  unreferenced_blocks()
+  unreferenced_blocks(superblock, group)
   invalid_blocks(superblock)
   allocated_blocks()
   duplicate_blocks()
